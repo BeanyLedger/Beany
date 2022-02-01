c9e2e7abf401ea7e2d023200a7f4c3b32410ec48
Vishesh Handa <me@vhanda.in>
2022-02-01 19:26:14 +0100

    Add a simple parsing script
    It outputs where it failed

---
+++ bin/parser.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:gringotts/parser.dart';
+
+int main(List<String> args) {
+  if (args.length != 1) {
+    print('usage: file');
+    return 1;
+  }
+
+  var input = File(args.first).readAsStringSync();
+  var pResult = parser.parse(input);
+  if (pResult.isFailure) {
+    print('Failure: $pResult');
+    var remaining = pResult.buffer.substring(pResult.position);
+    var lines = LineSplitter.split(remaining).toList();
+    print(lines[0]);
+    return 1;
+  }
+
+  return 0;
+}
