7043924a59f82fd551e7296f6cfb0fcab2bc266c
Vishesh Handa <me@vhanda.in>
2023-04-16 19:12:28 +0200

    Posting tests cleanup

--- test/parser/posting_test.dart
+++ test/parser/posting_test.dart
@@ -13,30 +13,26 @@ typedef A = Account;
 void main() {
   test('Posting Account Only Parser', () {
     var p = PostingSpec(A('Assets:Savings'), null);
-    expect(parse("  Assets:Savings").postingSpecAccountOnly().val(), p);
+    expect(parse("  Assets:Savings").postingSpec().val(), p);
   });
 
   test('Posting Full Parser', () {
     var p = PostingSpec(A("Expenses:Mystery:CatPowder"), AMT("1.5 EUR"));
     expect(
-        parse("  Expenses:Mystery:CatPowder  1.5 EUR")
-            .postingSpecAccountAmount()
-            .val(),
-        p);
+        parse("  Expenses:Mystery:CatPowder  1.5 EUR").postingSpec().val(), p);
   });
 
   test('Posting Full Parser Extra Spaces', () {
     var p = PostingSpec(A("Expenses:M"), AMT("1.5 EUR"));
-    expect(
-        parse("  Expenses:M     1.5 EUR").postingSpecAccountAmount().val(), p);
+    expect(parse("  Expenses:M     1.5 EUR").postingSpec().val(), p);
   });
 
   test('Posting Account Comment', () {
     var p = PostingSpec(A('Assets:Savings'), null, comment: "foo");
-    expect(parse("  Assets:Savings ; foo").postingSpecAccountOnly().val(), p);
+    expect(parse("  Assets:Savings ; foo").postingSpec().val(), p);
 
     expect(
-      parse("  Expenses:M  1.5 EUR ; foo").postingSpecAccountAmount().val(),
+      parse("  Expenses:M  1.5 EUR ; foo").postingSpec().val(),
       PostingSpec(A("Expenses:M"), AMT("1.5 EUR"), comment: "foo"),
     );
   });
@@ -44,14 +40,12 @@ void main() {
   test('Posting Account Tags', () {
     var p = PostingSpec(A('Assets:Savings'), null, tags: ['fire', 'water']);
     expect(
-      parse("  Assets:Savings #fire #water").postingSpecAccountOnly().val(),
+      parse("  Assets:Savings #fire #water").postingSpec().val(),
       p,
     );
 
     expect(
-      parse("  Expenses:M  1.5 EUR #ele-ment #gogo")
-          .postingSpecAccountAmount()
-          .val(),
+      parse("  Expenses:M  1.5 EUR #ele-ment #gogo").postingSpec().val(),
       PostingSpec(A("Expenses:M"), AMT("1.5 EUR"), tags: ["ele-ment", "gogo"]),
     );
   });
@@ -117,10 +111,4 @@ void main() {
     );
     expect(p, expected);
   });
-
-  // Posting with a cost spec per unit
-  //                cost spec total
-  //                price per unit
-  //                price total
-  //                price with number optional
 }
