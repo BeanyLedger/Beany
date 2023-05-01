65f389a7b6dd9d523b657eaabd781212106d9042
Vishesh Handa <me@vhanda.in>
2023-05-01 23:09:30 +0200

    beany-check: Load all the statements
    This was just operating on a single file instead of operating on the
    entire ledger with all the files.

--- bin/check.dart
+++ bin/check.dart
@@ -1,47 +1,24 @@
 import 'dart:io';
 
 import 'package:antlr4/antlr4.dart';
-import 'package:beany/core/transaction.dart';
-import 'package:beany/parser/parser.dart';
+import 'package:beany/engine/ledger.dart';
 
-int main(List<String> args) {
+Future<void> main(List<String> args) async {
   if (args.length != 1) {
     print('usage: file');
-    return 1;
+    exit(1);
   }
 
-  var input = File(args.first).readAsStringSync();
-
-  var parser = parse(input);
   try {
-    var statements = parser.all().val();
-    for (var st in statements) {
-      if (st is TransactionSpec) {
-        try {
-          st.resolve();
-        } catch (ex) {
-          print(ex);
-          exit(1);
-        }
-      }
-    }
+    var ledger = await Ledger.loadRootFile(args.first);
+    print("Found: ${ledger.statements.length} statements");
   } on ParseCancellationException catch (e) {
     print(e);
     print(e.stackTrace);
-    print(parser.currentToken);
     exit(1);
-  } catch (e) {
+  } catch (e, stackTrace) {
     print(e);
+    print(stackTrace);
     exit(1);
   }
-
-  /*if (pResult.isFailure) {
-    print('Failure: $pResult');
-    var remaining = pResult.buffer.substring(pResult.position);
-    var lines = LineSplitter.split(remaining).toList();
-    print(lines[0]);
-    return 1;
-  }*/
-
-  return 0;
 }
