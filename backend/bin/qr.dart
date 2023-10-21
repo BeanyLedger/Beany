2c452238555b65ae2dc7236fa199fc3f8b8be8a9
Vishesh Handa <me@vhanda.in>
2023-10-21 23:17:47 +0200

    Backend: Print out the server's address as a QR code
    This way we can easily scan it from the mobile app, and connect to the
    backend. We obviously aren't taking of NAT or anything else for now.

---
+++ backend/bin/qr.dart
@@ -0,0 +1,83 @@
+import 'package:qr/qr.dart';
+
+String printQr(String input) {
+  final qrCode = QrCode.fromData(
+    data: input,
+    errorCorrectLevel: QrErrorCorrectLevel.L,
+  );
+
+  var whiteB = '\x1b[30;107m';
+
+  var lowerB = '\u{2584}';
+  var upperB = '\u{2580}';
+  var fullB = '\u{2588}';
+  var empty = ' ';
+
+  var reset = "\x1b[m";
+
+  var qrImage = QrImage(qrCode);
+  var width = qrImage.moduleCount;
+  int margin = 2;
+
+  var output = '';
+
+  output += _margin(margin * 2, margin, width, whiteB + empty, reset);
+
+  for (int y = 0; y < width; y += 2) {
+    var line = '';
+    line += whiteB;
+
+    for (var x = 0; x < margin * 2; x++) {
+      line += empty;
+    }
+
+    for (int x = 0; x < width; x++) {
+      var isDark = qrImage.isDark(y, x);
+      var darkBelow = y + 1 == width ? false : qrImage.isDark(y + 1, x);
+      if (isDark) {
+        if (darkBelow) {
+          line += fullB;
+        } else {
+          line += upperB;
+        }
+      } else {
+        if (darkBelow) {
+          line += lowerB;
+        } else {
+          line += empty;
+        }
+      }
+    }
+
+    for (var x = 0; x < margin * 2; x++) {
+      line += empty;
+    }
+
+    line += reset;
+    output += '$line\n';
+  }
+
+  output += _margin(margin * 2, margin, width, whiteB + empty, reset);
+
+  return output.trimRight();
+}
+
+String _margin(
+  int horzMargin,
+  int verticalMargin,
+  int width,
+  String char,
+  String reset,
+) {
+  var output = '';
+  for (int y = 0; y < verticalMargin; y++) {
+    var line = '';
+    for (var x = 0; x < width + horzMargin * 2; x++) {
+      line += char;
+    }
+    line += reset;
+    output += line;
+    output += '\n';
+  }
+  return output;
+}
