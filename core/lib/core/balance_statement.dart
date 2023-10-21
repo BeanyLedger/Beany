190c32d75d2c2e3fb148ce206b8329b1e494ff9f
Vishesh Handa <me@vhanda.in>
2023-10-21 21:43:20 +0200

    core: Balance Statement: Remove unused fields
    Not sure what I was thinking when adding those fields

--- core/lib/core/balance_statement.dart
+++ core/lib/core/balance_statement.dart
@@ -19,24 +19,18 @@ class BalanceStatement extends Equatable implements Directive {
   final Account account;
   final Amount amount;
 
-  final Decimal? tolerance;
-  final Amount? diffAmount;
-
   final ParsingInfo? parsingInfo;
 
   BalanceStatement(
     this.date,
     this.account,
     this.amount, {
-    this.tolerance,
-    this.diffAmount,
     Map<String, dynamic>? meta,
     this.parsingInfo,
   }) : meta = IMap(meta);
 
   @override
-  List<Object?> get props =>
-      [date, meta, account, amount, tolerance, diffAmount];
+  List<Object?> get props => [date, meta, account, amount];
 
   @override
   bool get stringify => true;
