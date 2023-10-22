970b04188fa3f9f0f26ae91d36972969ffe926c0
Vishesh Handa <me@vhanda.in>
2023-10-22 04:55:05 +0200

    App: Change the font to a mono-spaced font
    I just thought it'll look better.
    
    * This required me to figure out why cocopod again doesn't work and
      debug it.
    
    * Podfiles now exist for Mac and iOS.
    
    * I've deliberately not committed the Podfile.lock files, as I already
      have a pubspec lock file and I don't see the point of having another
      one.

--- app/lib/src/app.dart
+++ app/lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
@@ -54,8 +55,8 @@ class MyApp extends StatelessWidget {
           // Define a light and dark color theme. Then, read the user's
           // preferred ThemeMode (light, dark, or system default) from the
           // SettingsController to display the correct theme.
-          theme: ThemeData(),
-          darkTheme: ThemeData.light(),
+          theme: _buildTheme(),
+          darkTheme: _buildTheme(),
           themeMode: settingsController.themeMode,
 
           // Define a function to handle named routes in order to support
@@ -76,4 +77,12 @@ class MyApp extends StatelessWidget {
       },
     );
   }
+}
+
+ThemeData _buildTheme() {
+  var base = ThemeData.light();
+  var textTheme = base.textTheme;
+  return base.copyWith(
+    textTheme: GoogleFonts.firaCodeTextTheme(textTheme),
+  );
 }
--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 
 {
-  packages = [ pkgs.antlr4 ];
+  packages = [ pkgs.antlr4 pkgs.cocoapods ];
 
   #languages.dart.enable = true;
+  languages.ruby.enable = true;
 }
