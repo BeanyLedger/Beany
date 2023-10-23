20549e8d344c1d3b12d5801a2d7673b88b75947f
Vishesh Handa <me@vhanda.in>
2023-10-23 21:17:52 +0200

    devenv: Remove cocapods + ruby
    This is only required in mac, and it fails to install in the CI which
    is a Linux based env
    
    The whole promise of only needing 1 env across different platforms with
    nix is turning out to be quite a lie.
    
    Ideally, I would just conditionally skip this on macos, but my nix
    skills are too low to do that.

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,11 @@
 { pkgs, ... }:
 
 {
-  packages = [ pkgs.antlr4 pkgs.cocoapods ];
+  packages = [ pkgs.antlr4 ];
+
+  # Enable for Macos
+  # packages = [ pkgs.antlr4 pkgs.cocoapods ];
+  # languages.ruby.enable = true;
 
   #languages.dart.enable = true;
-  languages.ruby.enable = true;
 }
